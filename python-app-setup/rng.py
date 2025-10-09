e0f42c19b6994c66a2e95a735f01152586f4f8f1
Svetlin Vatev <svetlinvatev@gmail.com>
2025-10-09 18:05:04 +0300

    testing

--- python-app-setup/rng.py
+++ python-app-setup/rng.py
@@ -4,8 +4,7 @@ min_number = int(input('Please enter the min number: '))
 max_number = int(input('Please enter the max number: '))
 
 if (max_number < min_number): 
-  print('Invalid input - shutting down...')
+  print('Invalid input - shutting down....')
 else:
   rnd_number = randint(min_number, max_number)
   print(rnd_number)
-  
